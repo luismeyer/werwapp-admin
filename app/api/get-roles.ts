95c608e26a9cc3768964ebcb51441e0f1b9ccd5e
Luis Meyer <luis.meyer@vercel.com>
2023-11-19 00:00:36 +0100

    fix: enable fetch revalidation

--- app/api/get-roles.ts
+++ app/api/get-roles.ts
@@ -1,9 +1,7 @@
 import { getBlobUrl } from "./get-blob-url";
 
 export async function getRoles() {
-  return fetch(getBlobUrl("roles.json"), {
-    next: { revalidate: false },
-  })
+  return fetch(getBlobUrl("roles.json"))
     .then((res) => res.json())
     .catch((error) => {
       console.log(error);
--- app/api/get-translations.ts
+++ app/api/get-translations.ts
@@ -8,7 +8,7 @@ export async function getTranslations(locale: string) {
   const pathname = getPathname(locale);
   const url = getBlobUrl(pathname);
 
-  return fetch(url, { next: { revalidate: false } })
+  return fetch(url)
     .then((res): Promise<Record<string, string>> => res.json())
     .catch((error) => {
       console.log(error);
